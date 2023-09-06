a507a41abbbe8561978c62f6fd59371024ee7544
GianniB77 <gbosio@GianniLaptop>
2023-09-06 09:26:25 +0200

    initial commit, FRONT/BACK

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+function App() {
+
+  const [data, setData] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Faites une requête GET à l'API
+    axios.get('http://localhost:5000/piece')
+      .then((response) => {
+        // Mettez à jour le state avec les données de l'API        
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des données :', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Contenu du tableau</h1>
+      <ul>
+        
+        {data.map((item: any) => (
+          <li key={item._id}>{item.nom}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const pieces = require('./models/pieces.js')
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.json())
+
+mongoose.connect("mongodb://mongo:27017/dockerconfirmed")
+  .then(() => {
+  console.log('Connecté à MongoDB');
+  })
+  .catch((error) => {
+  console.error(error);
+  })
+
+app.get("/piece", (req, res) => {
+    pieces.find().then( (piece) => {
+        res.send(piece);
+    }) 
+})
+
+app.post("/piece", (req, res) => {
+    let newPiece = new pieces({
+        nom: req.body.nom
+    })
+    newPiece.save();
+    res.status(201).send("Great success");
+})
+
+app.listen(5000, () => {console.log("Server started on port 5000")})
+
